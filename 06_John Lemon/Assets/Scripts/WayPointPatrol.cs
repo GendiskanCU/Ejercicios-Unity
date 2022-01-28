695e28feed86d6c85f957d82e296b3bcde10cf6b
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-01-28 17:58:24 +0100

    Finalización del script de la ruta NavMesh de los fantasmas

--- 06_John Lemon/Assets/Scripts/WayPointPatrol.cs
+++ 06_John Lemon/Assets/Scripts/WayPointPatrol.cs
@@ -11,6 +11,8 @@ public class WayPointPatrol : MonoBehaviour
 
     //Los diversos puntos de destino los guardaremos en un array o en una lista
     public Transform[] wayPoints;
+
+    private int currentWayPointIndex = 0;//Para controlar el siguiente waypoint al que deberá dirigirse
     
     // Start is called before the first frame update
     void Start()
@@ -19,13 +21,22 @@ public class WayPointPatrol : MonoBehaviour
         navMeshAgent = GetComponent<NavMeshAgent>();
         
         //Enviamos hacia el primer waypoint
-        navMeshAgent.SetDestination(wayPoints[0].position);
-
+        navMeshAgent.SetDestination(wayPoints[currentWayPointIndex].position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Cuando haya llegado al destino (waypoint) actual
+        //(se puede comprobar si la distancia restante es menor que la distancia de parado, que está en 0.2)
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            //Calculamos el próximo waypoint, debiendo volver al primero cuando alcance ya el último de la lista
+            //Con esta fórmula aseguramos que el valor siempre estará entre 0 y Length-1, sin tener que usar "If"
+            currentWayPointIndex = (currentWayPointIndex + 1) % wayPoints.Length;
+            //Y enviamos al nuevo waypoint
+            navMeshAgent.SetDestination(wayPoints[currentWayPointIndex].position);
+        }
     }
+
 }
